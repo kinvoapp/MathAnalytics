d150c1bce8a7ba3918ab601a799a11dfaf8865cb
Mateus Lopes <lopes19@gmail.com>
2017-11-06 13:40:56 -0300

    Change test methods for AccumulateInterest and CalculateCompoundInterest

--- MathAnalytics.Test/Extensions/AccumulateInterestTest.cs
+++ MathAnalytics.Test/Extensions/AccumulateInterestTest.cs
@@ -8,7 +8,7 @@ namespace MathAnalytics.Test.Extensions
         [Theory]
         [InlineData(0, 0, 0)]
         [InlineData(1.5, 15, 16.72500)]
-        public void DecimalExtension(decimal currentInterest, decimal cumulativeInterest, decimal resultExpected)
+        public void AccumulateInterest_Decimal_GivenCumulativeInterestAndCurrentInterest_NewCumulativeInterestReturned(decimal currentInterest, decimal cumulativeInterest, decimal resultExpected)
         {
             cumulativeInterest.AccumulateInterest(currentInterest).Should().Be(resultExpected);
         }
@@ -16,7 +16,7 @@ namespace MathAnalytics.Test.Extensions
         [Theory]
         [InlineData(0, 0, 0)]
         [InlineData(1.5, 15, 16.724999999999991)]
-        public void DoubleExtension(double currentInterest, double cumulativeInterest, double resultExpected)
+        public void AccumulateInterest_Double_GivenCumulativeInterestAndCurrentInterest_NewCumulativeInterestReturned(double currentInterest, double cumulativeInterest, double resultExpected)
         {
             cumulativeInterest.AccumulateInterest(currentInterest).Should().Be(resultExpected);
         }
@@ -24,7 +24,7 @@ namespace MathAnalytics.Test.Extensions
         [Theory]
         [InlineData(0, 0, 0)]
         [InlineData(1.5, 15, 16.72499)]
-        public void FloatExtension(float currentInterest, float cumulativeInterest, float resultExpected)
+        public void AccumulateInterest_Float_GivenCumulativeInterestAndCurrentInterest_NewCumulativeInterestReturned(float currentInterest, float cumulativeInterest, float resultExpected)
         {
             cumulativeInterest.AccumulateInterest(currentInterest).Should().Be(resultExpected);
         }
--- MathAnalytics.Test/Extensions/CalculateCompoundInterestTest.cs
+++ MathAnalytics.Test/Extensions/CalculateCompoundInterestTest.cs
@@ -8,7 +8,7 @@ namespace MathAnalytics.Test.Extensions
         [Theory]
         [InlineData(0, 0, 0, 0)]
         [InlineData(10000, 1.5, 50, 21052.4242060746)]
-        public void DecimalExtension(decimal principal, decimal interestRate, int numberOfPoints, decimal resultExpected)
+        public void CalculateCompoundInterest_Decimal_GivenPrincipalAndInterestRateAndNumberOfPoints_CompoundInterestReturned(decimal principal, decimal interestRate, int numberOfPoints, decimal resultExpected)
         {
             principal.CalculateCompoundInterest(interestRate, numberOfPoints).Should().Be(resultExpected);
         }
@@ -16,7 +16,7 @@ namespace MathAnalytics.Test.Extensions
         [Theory]
         [InlineData(0, 0, 0, 0)]
         [InlineData(10000, 1.5, 50, 21052.424206074647)]
-        public void DoubleExtension(double principal, double interestRate, int numberOfPoints, double resultExpected)
+        public void CalculateCompoundInterest_Double_GivenPrincipalAndInterestRateAndNumberOfPoints_CompoundInterestReturned(double principal, double interestRate, int numberOfPoints, double resultExpected)
         {
             principal.CalculateCompoundInterest(interestRate, numberOfPoints).Should().Be(resultExpected);
         }
@@ -24,7 +24,7 @@ namespace MathAnalytics.Test.Extensions
         [Theory]
         [InlineData(0, 0, 0, 0)]
         [InlineData(10000, 1.5, 50, 21052.4082)]
-        public void FloatExtension(float principal, float interestRate, int numberOfPoints, float resultExpected)
+        public void CalculateCompoundInterest_Float_GivenPrincipalAndInterestRateAndNumberOfPoints_CompoundInterestReturned(float principal, float interestRate, int numberOfPoints, float resultExpected)
         {
             principal.CalculateCompoundInterest(interestRate, numberOfPoints).Should().Be(resultExpected);
         }
