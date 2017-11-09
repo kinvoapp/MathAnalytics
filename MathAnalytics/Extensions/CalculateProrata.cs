95e125fba1490d546695a88216c37fb77a08d8d7
Mateus Lopes <lopes19@gmail.com>
2017-11-08 22:59:07 -0300

    Rename a single parameter

--- MathAnalytics/Extensions/CalculateProrata.cs
+++ MathAnalytics/Extensions/CalculateProrata.cs
@@ -9,28 +9,28 @@ namespace MathAnalytics
     // See the LICENSE file in the project root for more information.
     public static partial class Extensions
     {
-        public static decimal CalculateProrata(this decimal value, decimal interestRate, int days)
+        public static decimal CalculateProrata(this decimal value, decimal interestRate, int numberOfDays)
         {
-            if (days == 0)
+            if (numberOfDays == 0)
                 return 0;
 
-            return Convert.ToDecimal(Math.Pow(1 + ((double)interestRate / 100), 1 / (double)days) - 1) * 100;
+            return Convert.ToDecimal(Math.Pow(1 + ((double)interestRate / 100), 1 / (double)numberOfDays) - 1) * 100;
         }
 
-        public static double CalculateProrata(this double value, double interestRate, int days)
+        public static double CalculateProrata(this double value, double interestRate, int numberOfDays)
         {
-            if (days == 0)
+            if (numberOfDays == 0)
                 return 0;
 
-            return (Math.Pow(1 + interestRate / 100, 1 / (double)days) - 1) * 100;
+            return (Math.Pow(1 + interestRate / 100, 1 / (double)numberOfDays) - 1) * 100;
         }
 
-        public static float CalculateProrata(this float value, float interestRate, int days)
+        public static float CalculateProrata(this float value, float interestRate, int numberOfDays)
         {
-            if (days == 0)
+            if (numberOfDays == 0)
                 return 0;
 
-            return (float)(Math.Pow(1 + ((double)interestRate / 100), 1 / (double)days) - 1) * 100;
+            return (float)(Math.Pow(1 + ((double)interestRate / 100), 1 / (double)numberOfDays) - 1) * 100;
         }
     }
 }
