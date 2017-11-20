8065ebdd729a8c963df8efbaaed96a235e156a83
Mateus Lopes <lopes19@gmail.com>
2017-11-20 16:32:38 -0300

    Including LinerRegression method

---
+++ MathAnalytics.Test/Extensions/LinearRegressionTest.cs
@@ -0,0 +1,276 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MathAnalytics.Test.Extensions
+{
+    public class LinearRegressionTest
+    {
+        #region SourceIsNull - ArgumentNullExceptionThrown
+
+        [Fact]
+        public void LinearRegressionOfDecimalList_SourceIsNull_ArgumentNullExceptionThrown()
+        {
+            decimal[] listX = null;
+            decimal[] listY = new decimal[] { 0, 1, 2.5M, 4.8M, 6.0M, 5.8M };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<ArgumentNullException>();
+
+            listX = new decimal[] { 0, 1, 2, 3, 4, 5 };
+            listY = null;
+
+            comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+
+            comparison.ShouldThrow<ArgumentNullException>();
+        }
+
+
+        [Fact]
+        public void LinearRegressionOfDoubleList_SourceIsNull_ArgumentNullExceptionThrown()
+        {
+            double[] listX = null;
+            double[] listY = new double[] { 0, 1, 2.5, 4.8, 6.0, 5.8 };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<ArgumentNullException>();
+
+            listX = new double[] { 0, 1, 2, 3, 4, 5 };
+            listY = null;
+
+            comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+
+            comparison.ShouldThrow<ArgumentNullException>();
+        }
+
+
+        [Fact]
+        public void LinearRegressionOfFloatList_SourceIsNull_ArgumentNullExceptionThrown()
+        {
+            float[] listX = null;
+            float[] listY = new float[] { 0, 1, 2.5F, 4.8F, 6.0F, 5.8F };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<ArgumentNullException>();
+
+            listX = new float[] { 0, 1, 2, 3, 4, 5 };
+            listY = null;
+
+            comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+
+            comparison.ShouldThrow<ArgumentNullException>();
+        }
+
+        #endregion
+
+        #region SourceIsEmptyCollection - InvalidOperationExceptionThrown
+
+        [Fact]
+        public void LinearRegressionOfDecimalList_SourceIsEmptyCollection_InvalidOperationExceptionThrown()
+        {
+            decimal[] listX = new decimal[] { };
+            decimal[] listY = new decimal[] { };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void LinearRegressionOfDoubleList_SourceIsEmptyCollection_InvalidOperationExceptionThrown()
+        {
+            double[] listX = new double[] { };
+            double[] listY = new double[] { };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<InvalidOperationException>();
+        }
+
+        [Fact]
+        public void LinearRegressionOfFloatList_SourceIsEmptyCollection_InvalidOperationExceptionThrown()
+        {
+            float[] listX = new float[] { };
+            float[] listY = new float[] { };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<InvalidOperationException>();
+        }
+
+        #endregion
+
+        #region SourceIsNotEmptyAndTheSameLength -  LinearRegressionReturned
+
+        [Fact]
+        public void LinearRegressionOfDecimalList_SourceIsNotEmptyAndTheSameLength_LinearRegressionReturned()
+        {
+            var listX = new decimal[] { 0, 1, 2, 3, 4, 5 };
+            var listY = new decimal[] { 0, 1, 2.5M, 4.8M, 6.0M, 5.8M };
+
+            var result = listX.LinearRegression(listY);
+
+            result.RSquared.Should().Be(0.9395349856463521406241478997M);
+            result.YIntercept.Should().Be(0.0428571428571428571428571428M);
+            result.Slope.Should().Be(1.3228571428571428571428571429M);
+        }
+
+        [Fact]
+        public void LinearRegressionOfDoubleList_SourceIsNotEmptyAndTheSameLength_LinearRegressionReturned()
+        {
+            var listX = new double[] { 0, 1, 2, 3, 4, 5 };
+            var listY = new double[] { 0, 1, 2.5, 4.8, 6.0, 5.8 };
+
+            var result = listX.LinearRegression(listY);
+
+            result.RSquared.Should().Be(0.9395349856463534);
+            result.YIntercept.Should().Be(0.042857142857142261);
+            result.Slope.Should().Be(1.3228571428571432);
+        }
+
+        [Fact]
+        public void LinearRegressionOfFloatList_SourceIsNotEmptyAndTheSameLength_LinearRegressionReturned()
+        {
+            var listX = new float[] { 0, 1, 2, 3, 4, 5 };
+            var listY = new float[] { 0, 1, 2.5F, 4.8F, 6.0F, 5.8F };
+
+            var result = listX.LinearRegression(listY);
+
+            result.RSquared.Should().Be(0.9395349F);
+            result.YIntercept.Should().Be(0.04285693F);
+            result.Slope.Should().Be(1.32285726F);
+        }
+
+        #endregion
+
+        #region SourceIsNotEmptyButDifferentLengths -  LinearRegressionReturned
+
+        [Fact]
+        public void LinearRegressionOfDecimalList_SourceIsNotEmptyButDifferentLengths_ArgumentExceptionThrown()
+        {
+            var listX = new decimal[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            var listY = new decimal[] { 0, 1, 2.5M, 4.8M, 6.0M, 5.8M };
+
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void LinearRegressionOfDoubleList_SourceIsNotEmptyButDifferentLengths_ArgumentExceptionThrown()
+        {
+            var listX = new double[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            var listY = new double[] { 0, 1, 2.5, 4.8, 6.0, 5.8 };
+
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<ArgumentException>();
+        }
+
+        [Fact]
+        public void LinearRegressionOfFloatList_SourceIsNotEmptyButDifferentLengths_ArgumentExceptionThrown()
+        {
+            var listX = new float[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            var listY = new float[] { 0, 1, 2.5F, 4.8F, 6.0F, 5.8F };
+
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<ArgumentException>();
+        }
+
+        #endregion
+
+        #region SourceCumulativePercentageToOverflow - OverflowExceptionThrown or Infinity returned
+
+        [Fact]
+        public void LinearRegressionOfDecimalList_SourceCumulativePercentageToOverflow_OverflowExceptionThrown()
+        {
+            var listX = new decimal[] { decimal.MaxValue, decimal.MaxValue };
+            var listY = new decimal[] { decimal.MaxValue, decimal.MaxValue };
+
+            Action comparison = () =>
+            {
+                listX.LinearRegression(listY);
+            };
+
+            comparison.ShouldThrow<OverflowException>();
+        }
+
+        [Fact]
+        public void LinearRegressionOfDoubleList_SourceCumulativePercentageToOverflow_NotInfinityReturned()
+        {
+            var listX = new double[] { double.MaxValue, double.MaxValue };
+            var listY = new double[] { double.MaxValue, double.MaxValue };
+
+            var result = listX.LinearRegression(listY);
+
+            double.IsPositiveInfinity(result.RSquared).Should().BeFalse();
+            double.IsPositiveInfinity(result.YIntercept).Should().BeFalse();
+            double.IsPositiveInfinity(result.Slope).Should().BeFalse();
+        }
+
+        [Fact]
+        public void LinearRegressionOfFloatList_SourceCumulativePercentageToOverflow_NotInfinityReturned()
+        {
+
+            IEnumerable<float> measure1 = new float[] { float.MaxValue, float.MaxValue };
+            IEnumerable<float> measure2 = new float[] { float.MaxValue, float.MaxValue };
+
+            var result = measure1.LinearRegression(measure2);
+
+            float.IsPositiveInfinity(result.RSquared).Should().BeFalse();
+            float.IsPositiveInfinity(result.YIntercept).Should().BeFalse();
+            float.IsPositiveInfinity(result.Slope).Should().BeFalse();
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ MathAnalytics/Extensions/LinearRegression.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MathAnalytics
+{
+    public class LinearRegressionTuple<T1, T2, T3> : Tuple<T1, T2, T3>
+    {
+        public LinearRegressionTuple(T1 rsquared, T2 yintercept, T3 slope)
+                : base(rsquared, yintercept, slope)
+        {
+
+        }
+
+        public T1 RSquared { get { return this.Item1; } }
+        public T2 YIntercept { get { return this.Item2; } }
+        public T3 Slope { get { return this.Item3; } }
+
+    }
+
+    // Licensed to the .NET Foundation under one or more agreements.
+    // The .NET Foundation licenses this file to you under the MIT license.
+    // See the LICENSE file in the project root for more information.
+    public static partial class Extensions
+    {
+        public static LinearRegressionTuple<decimal, decimal, decimal> LinearRegression(this IEnumerable<decimal> xVals, IEnumerable<decimal> yVals)
+        {
+            if (xVals == null)
+            {
+                throw new ArgumentNullException(nameof(xVals));
+            }
+
+            if (yVals == null)
+            {
+                throw new ArgumentNullException(nameof(yVals));
+            }
+
+            if (xVals.Count() != yVals.Count())
+            {
+                throw new ArgumentException(nameof(yVals));
+            }
+
+            if (xVals.Count() == 0)
+            {
+                throw new InvalidOperationException(nameof(yVals));
+            }
+
+            decimal sumOfX = 0;
+            decimal sumOfY = 0;
+            decimal sumOfXSq = 0;
+            decimal sumOfYSq = 0;
+            decimal ssX = 0;
+            decimal ssY = 0;
+            decimal sumCodeviates = 0;
+            decimal sCo = 0;
+            decimal count = xVals.Count();
+
+            for (int ctr = 0; ctr < count; ctr++)
+            {
+                var x = xVals.ElementAt(ctr);
+                var y = yVals.ElementAt(ctr);
+                sumCodeviates += x * y;
+                sumOfX += x;
+                sumOfY += y;
+                sumOfXSq += x * x;
+                sumOfYSq += y * y;
+            }
+            ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
+            ssY = sumOfYSq - ((sumOfY * sumOfY) / count);
+            var RNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
+            var RDenom = (count * sumOfXSq - (sumOfX * sumOfX))
+             * (count * sumOfYSq - (sumOfY * sumOfY));
+            sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
+
+            var meanX = sumOfX / count;
+            var meanY = sumOfY / count;
+            var dblR = Convert.ToDecimal((double)RNumerator / Math.Sqrt((double)RDenom));
+
+            var rsquared = dblR * dblR;
+            var yintercept = meanY - ((sCo / ssX) * meanX);
+            var slope = sCo / ssX;
+
+            return new LinearRegressionTuple<decimal, decimal, decimal>(rsquared, yintercept, slope);
+        }
+
+        public static LinearRegressionTuple<double, double, double> LinearRegression(this IEnumerable<double> xVals, IEnumerable<double> yVals)
+        {
+            if (xVals == null)
+            {
+                throw new ArgumentNullException(nameof(xVals));
+            }
+
+            if (yVals == null)
+            {
+                throw new ArgumentNullException(nameof(yVals));
+            }
+
+            if (xVals.Count() != yVals.Count())
+            {
+                throw new ArgumentException(nameof(yVals));
+            }
+
+            if (xVals.Count() == 0)
+            {
+                throw new InvalidOperationException(nameof(yVals));
+            }
+
+            double sumOfX = 0;
+            double sumOfY = 0;
+            double sumOfXSq = 0;
+            double sumOfYSq = 0;
+            double ssX = 0;
+            double ssY = 0;
+            double sumCodeviates = 0;
+            double sCo = 0;
+            double count = xVals.Count();
+
+            for (int ctr = 0; ctr < count; ctr++)
+            {
+                var x = xVals.ElementAt(ctr);
+                var y = yVals.ElementAt(ctr);
+                sumCodeviates += x * y;
+                sumOfX += x;
+                sumOfY += y;
+                sumOfXSq += x * x;
+                sumOfYSq += y * y;
+            }
+            ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
+            ssY = sumOfYSq - ((sumOfY * sumOfY) / count);
+            var RNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
+            var RDenom = (count * sumOfXSq - (sumOfX * sumOfX))
+             * (count * sumOfYSq - (sumOfY * sumOfY));
+            sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
+
+            var meanX = sumOfX / count;
+            var meanY = sumOfY / count;
+            var dblR = RNumerator / Math.Sqrt(RDenom);
+
+            var rsquared = dblR * dblR;
+            var yintercept = meanY - ((sCo / ssX) * meanX);
+            var slope = sCo / ssX;
+
+            return new LinearRegressionTuple<double, double, double>(rsquared, yintercept, slope);
+
+        }
+
+        public static LinearRegressionTuple<float, float, float> LinearRegression(this IEnumerable<float> xVals, IEnumerable<float> yVals)
+        {
+            if (xVals == null)
+            {
+                throw new ArgumentNullException(nameof(xVals));
+            }
+
+            if (yVals == null)
+            {
+                throw new ArgumentNullException(nameof(yVals));
+            }
+
+            if (xVals.Count() != yVals.Count())
+            {
+                throw new ArgumentException(nameof(yVals));
+            }
+
+            if (xVals.Count() == 0)
+            {
+                throw new InvalidOperationException(nameof(yVals));
+            }
+
+            float sumOfX = 0;
+            float sumOfY = 0;
+            float sumOfXSq = 0;
+            float sumOfYSq = 0;
+            float ssX = 0;
+            float ssY = 0;
+            float sumCodeviates = 0;
+            float sCo = 0;
+            float count = xVals.Count();
+
+            for (int ctr = 0; ctr < count; ctr++)
+            {
+                var x = xVals.ElementAt(ctr);
+                var y = yVals.ElementAt(ctr);
+                sumCodeviates += x * y;
+                sumOfX += x;
+                sumOfY += y;
+                sumOfXSq += x * x;
+                sumOfYSq += y * y;
+            }
+            ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
+            ssY = sumOfYSq - ((sumOfY * sumOfY) / count);
+            var RNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
+            var RDenom = (count * sumOfXSq - (sumOfX * sumOfX))
+             * (count * sumOfYSq - (sumOfY * sumOfY));
+            sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
+
+            var meanX = sumOfX / count;
+            var meanY = sumOfY / count;
+            var dblR = (float)(RNumerator / Math.Sqrt(RDenom));
+
+            var rsquared = dblR * dblR;
+            var yintercept = meanY - ((sCo / ssX) * meanX);
+            var slope = sCo / ssX;
+
+            return new LinearRegressionTuple<float, float, float>(rsquared, yintercept, slope);
+        }
+    }
+}
